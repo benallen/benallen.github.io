60009ce668771050b8f776524796ee9c8b26be1a
Ben Allen <benallen1@gmail.com>
2017-03-07 16:14:13 -0800

    Basic rails app

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('install!');
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('activate!');
-  // You're good to go!
-});
